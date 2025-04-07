592df80d39db4fbd6fa9fbbecd20ef2be16e0b39
jtabke <25010496+jtabke@users.noreply.github.com>
2025-04-06 21:26:08 -0700

    update lsp config in nvim

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,116 @@
+return {
+	-- Autocompletion
+	{
+		"saghen/blink.cmp",
+		-- optional: provides snippets for the snippet source
+		dependencies = { "rafamadriz/friendly-snippets" },
+		-- use a release tag to download pre-built binaries
+		version = "1.*",
+		opts = {
+			keymap = { preset = "default" },
+			appearance = {
+				nerd_font_variant = "mono",
+			},
+			-- (Default) Only show the documentation popup when manually triggered
+			completion = { documentation = { auto_show = false } },
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		opts_extend = { "sources.default" },
+	},
+
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+		cmd = { "LspInfo", "LspInstall", "LspStart" },
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+		},
+		init = function()
+			vim.opt.signcolumn = "yes" -- Show sign column always
+
+			-- Configure diagnostics with signs defined directly
+			vim.diagnostic.config({
+				virtual_text = false, -- Set to true or a table for inline diagnostics
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = " ",
+						[vim.diagnostic.severity.WARN] = " ",
+						[vim.diagnostic.severity.HINT] = " ",
+						[vim.diagnostic.severity.INFO] = " ",
+					},
+					texthl = {
+						[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+						[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+						[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+						[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+					},
+					numhl = {
+						[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+						[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+						[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+						[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+					},
+				},
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+				float = { border = "rounded", source = "always" },
+			})
+		end,
+		config = function()
+			-- LSP capabilities from blink.cmp
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+			vim.api.nvim_create_autocmd("LspAttach", {
+				desc = "LSP actions",
+				callback = function(event)
+					local opts = { buffer = event.buf }
+
+					vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+					vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+					vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+					vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+					vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+					vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+					vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+					vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+					vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+					vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+					vim.keymap.set(
+						"n",
+						"]d",
+						vim.diagnostic.goto_next,
+						vim.tbl_extend("force", opts, { desc = "Go to next diagnostic" })
+					)
+					vim.keymap.set(
+						"n",
+						"[d",
+						vim.diagnostic.goto_prev,
+						vim.tbl_extend("force", opts, { desc = "Go to previous diagnostic" })
+					)
+				end,
+			})
+			require("mason").setup() -- Ensure this is loaded before mason-lspconfig
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ruff", "pyright", "tailwindcss", "html" },
+				handlers = {
+					function(server_name)
+						require("lspconfig")[server_name].setup({ capabilities = capabilities })
+					end,
+					["lua_ls"] = function()
+						require("lspconfig").lua_ls.setup({
+							settings = {
+								Lua = {
+									diagnostics = {
+										globals = { "vim" },
+									},
+								},
+							},
+						})
+					end,
+				},
+			})
+		end,
+	},
+}
